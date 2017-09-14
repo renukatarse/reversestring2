d935f8ec8dc1d66ddc13061ffeb0f22a11b0eff9
renuka <renutarse1@gmail.com>
2017-09-14 16:54:03 +0530

    reverse2 commit

---
+++ reversestring2/src/reversestring2/reverseString.java
@@ -0,0 +1,19 @@
+package reversestring2;
+import java.io.*;
+import java.util.*;
+
+public class reverseString {
+	public static void main(String[] args) {
+		  String input="AliveisAwesome";
+		  StringBuilder input1 = new StringBuilder();
+		  input1.append(input);
+		  input1=input1.reverse(); 
+		  for (int i=0;i<input1.length();i++)
+		  System.out.print(input1.charAt(i));  
+		 }}
+		  
+
+
+
+
+
