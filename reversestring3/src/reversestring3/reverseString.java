d9262c0a801daec8fa4c3751e7348bcfc02fe068
renuka <renutarse1@gmail.com>
2017-09-14 16:56:20 +0530

    reversestring3 commit

---
+++ reversestring3/src/reversestring3/reverseString.java
@@ -0,0 +1,25 @@
+package reversestring3;
+import java.io.*;
+import java.util.*;
+
+
+public class reverseString {
+	
+      public static void main(String[] args) {
+    String input = "Be in present";
+    char[] temparray= input.toCharArray();
+    int left,right=0;
+    right=temparray.length-1;
+    for (left=0; left < right ; left++ ,right--)
+    {
+     // Swap values of left and right 
+     char temp = temparray[left];
+     temparray[left] = temparray[right];
+     temparray[right]=temp;
+    }
+    for (char c : temparray)
+     System.out.print(c);
+    System.out.println();
+   }}
+  
+
